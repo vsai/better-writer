4a05fbf67415ca467f4de6d77fa7f27248e0e70e
vsai <vishalsai@gmail.com>
2021-04-18 09:51:30 +0800

    highlighter: remove old dead code

--- highlighter/highlighter.js
+++ highlighter/highlighter.js
@@ -107,57 +107,24 @@ const options = {
     // return pressedKeys.includes('Alt'); // Option key
     return true;
   },
-  // occurrenceRegex: function (selectionString) {
-  //   return new RegExp(selectionString, 'i'); // partial word, case insensitive
-  //   // return new RegExp(selectionString); // partial word, case sensitive
-  //   // return new RegExp(`\\b${selectionString}\\b`, 'i'); // whole word, case insensitive
-  //   // return new RegExp(`\\b${selectionString}\\b`); // whole word, case sensitive
-  // },
-  // isAncestorNodeValid: function isAncestorNodeValid(ancestorNode) {
-  //   return !ancestorNode || (!ancestorNode.classList || !ancestorNode.classList.contains('CodeMirror')) && ancestorNode.nodeName !== 'SCRIPT' && ancestorNode.nodeName !== 'STYLE' && ancestorNode.nodeName !== 'HEAD' && ancestorNode.nodeName !== 'TITLE' && ancestorNode.nodeName !== 'INPUT' && ancestorNode.nodeName !== 'TEXTAREA' && ancestorNode.contentEditable !== 'true' && isAncestorNodeValid(ancestorNode.parentNode);
-  // },
   trimRegex: function () {
     // leading, selectionString, trailing
     // trim parts maintained for offset analysis
     return /^(\s*)(\S+(?:\s+\S+)*)(\s*)$/;
   },
-  // highlightedClassName: 'highlighted_selection',
-  // styles: {
-  //   backgroundColor: 'rgb(255,255,0,1)',
-  //   // yellow 100%
-  //   margin: '0',
-  //   padding: '0',
-  //   lineHeight: '1' // display: 'inline',
-  //
-  // },
-  // areScrollMarkersEnabled: function () {
-  //   return true;
-  // },
-  // scrollMarkerClassName: 'highlighted_selection_scroll_marker',
-  // scrollMarkerStyles: function ({
-  //   window,
-  //   document,
-  //   highlightedNode
-  // }) {
-  //   const clientRect = highlightedNode.getBoundingClientRect();
-  //
-  //   if (!clientRect.width || !clientRect.height) {
-  //     return false;
-  //   }
-  //
-  //   return {
-  //     height: '2px',
-  //     width: '16px',
-  //     boxSizing: 'content-box',
-  //     border: '1px solid grey',
-  //     position: 'fixed',
-  //     top: // window height times percent of element position in document
-  //     window.innerHeight * (+window.scrollY + clientRect.top + 0.5 * (clientRect.top - clientRect.bottom)) / document.body.clientHeight + 'px',
-  //     right: '0px',
-  //     backgroundColor: 'yellow',
-  //     zIndex: '2147483647'
-  //   };
-  // }
+  styles: {
+    backgroundColor: 'green',
+    margin: '10px',
+    padding: '10px',
+    borderStyle: 'solid',
+    borderColor: 'red',
+    borderWidth: '2px',
+    position: 'absolute',
+    top: '10px',
+    left: '10px',
+    height: '200px',
+    width: '200px',
+  },
 };
 chrome.storage.sync.get('optionsText', e => {
   if (e.optionsText) {
@@ -176,11 +143,15 @@ chrome.storage.sync.get('optionsText', e => {
 
 function initialize() {
   console.log("BetterWriter: starting initialize()")
-  // const highlightedMarkTemplate = document.createElement('mark');
-  // highlightedMarkTemplate.className = options.highlightedClassName;
-  // Object.entries(options.styles).forEach(([styleName, styleValue]) => {
-  //   highlightedMarkTemplate.style[styleName] = styleValue;
-  // });
+
+  const searchResultsNode = document.createElement('div');
+  Object.entries(options.styles).forEach(([styleName, styleValue]) => {
+    searchResultsNode.style[styleName] = styleValue;
+  });
+  const textnode = document.createTextNode("BetterWriter.ai");
+  searchResultsNode.appendChild(textnode);
+  document.body.appendChild(searchResultsNode);
+
   const pressedKeys = [];
   document.addEventListener('keydown', e => {
     const index = pressedKeys.indexOf(e.key);
@@ -206,32 +177,10 @@ function initialize() {
     latestStartTime = performance.now();
     if (!options.isWindowLocationValid(window.location)) return;
     if (!options.areKeysPressed(pressedKeys)) return;
-    removeThenHighlight(latestStartTime);
+    highlight(latestStartTime);
   }
 
   ;
-
-  function removeThenHighlight(startTime) {
-    // requestAnimationFrame(() => {
-    //   if (startTime !== latestStartTime) return;
-    //   document.querySelectorAll('.' + options.highlightedClassName).forEach(element => {
-    //     const parent = element.parentNode;
-    //
-    //     if (parent) {
-    //       parent.replaceChild(new Text(element.textContent || ''), element);
-    //       parent.normalize();
-    //     }
-    //   });
-    //
-    //   if (options.areScrollMarkersEnabled()) {
-    //     document.querySelectorAll('.' + options.scrollMarkerClassName).forEach(element => {
-    //       document.body.removeChild(element);
-    //     });
-    //   }
-
-      highlight(startTime);
-    // });
-  }
 
   function highlight(startTime) {
     const selection = document.getSelection();
@@ -246,85 +195,6 @@ function initialize() {
     })) return; // https://stackoverflow.com/questions/3561493/is-there-a-regexp-escape-function-in-javascript
 
     console.log("highlighted valid selected text", selectionString);
-
-    // const occurrenceRegex = options.occurrenceRegex(selectionString.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&'));
-    // const allTextNodes = [];
-    // const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT, null, false);
-    //
-    // while (treeWalker.nextNode()) {
-    //   allTextNodes.push(treeWalker.currentNode);
-    // }
-
-    // for (let i = 0; i < allTextNodes.length; i++) {
-    //   const textNode = allTextNodes[i];
-    //   const parent = textNode.parentNode;
-    //   const highlightedNodes = highlightOccurrences(textNode);
-    //
-    //   if (highlightedNodes) {
-    //     if (parent) parent.normalize();
-    //   }
-    //
-    //   ;
-    // }
-
-    // if (options.areScrollMarkersEnabled()) {
-    //   requestAnimationFrame(() => {
-    //     if (startTime !== latestStartTime) return;
-    //     const highlighted = document.querySelectorAll('.' + options.highlightedClassName);
-    //     const scrollMarkersFragment = document.createDocumentFragment();
-    //
-    //     for (let i = 0; i < highlighted.length; i++) {
-    //       setTimeout(() => {
-    //         if (startTime !== latestStartTime) return;
-    //         const highlightedNode = highlighted[i];
-    //         const scrollMarker = document.createElement('div');
-    //         scrollMarker.className = options.scrollMarkerClassName;
-    //         const scrollMarkerStyles = options.scrollMarkerStyles({
-    //           window,
-    //           document,
-    //           highlightedNode
-    //         });
-    //
-    //         if (scrollMarkerStyles) {
-    //           Object.entries(scrollMarkerStyles).forEach(([styleName, styleValue]) => {
-    //             scrollMarker.style[styleName] = styleValue;
-    //           });
-    //           scrollMarkersFragment.appendChild(scrollMarker);
-    //         }
-    //       }, 0);
-    //     }
-    //
-    //     setTimeout(() => {
-    //       if (startTime === latestStartTime) {
-    //         document.body.appendChild(scrollMarkersFragment);
-    //       }
-    //     }, 0);
-    //   });
-    // }
-
-    // function highlightOccurrences(textNode) {
-    //   const match = occurrenceRegex.exec(textNode.data);
-    //   if (!match) return;
-    //   if (!options.isAncestorNodeValid(textNode.parentNode)) return;
-    //   const matchIndex = match.index;
-    //   const anchorToFocusDirection = selection.anchorNode.compareDocumentPosition(selection.focusNode);
-    //   const isUsersSelection = anchorToFocusDirection & Node.DOCUMENT_POSITION_FOLLOWING ? textNode === selection.anchorNode && (selection.anchorNode.nodeType === Node.ELEMENT_NODE && selection.anchorOffset === 0 || selection.anchorOffset === matchIndex - leadingSpaces.length) || textNode === selection.focusNode && (selection.focusNode.nodeType === Node.ELEMENT_NODE && selection.focusOffset === 0 || selection.focusOffset === matchIndex + selectionString.length + trailingSpaces.length) || textNode !== selection.anchorNode && textNode !== selection.focusNode && selection.anchorNode.compareDocumentPosition(textNode) & Node.DOCUMENT_POSITION_FOLLOWING && selection.focusNode.compareDocumentPosition(textNode) & Node.DOCUMENT_POSITION_PRECEDING : anchorToFocusDirection & Node.DOCUMENT_POSITION_PRECEDING ? textNode === selection.anchorNode && (selection.anchorNode.nodeType === Node.ELEMENT_NODE && selection.anchorOffset === 0 || selection.anchorNode.nodeType === Node.TEXT_NODE && selection.anchorOffset === matchIndex + selectionString.length + trailingSpaces.length) || textNode === selection.focusNode && (selection.focusNode.nodeType === Node.ELEMENT_NODE && selection.focusOffset === 0 || selection.focusOffset === matchIndex - leadingSpaces.length) || textNode !== selection.anchorNode && textNode !== selection.focusNode && selection.anchorNode.compareDocumentPosition(textNode) & Node.DOCUMENT_POSITION_PRECEDING && selection.focusNode.compareDocumentPosition(textNode) & Node.DOCUMENT_POSITION_FOLLOWING : selection.anchorOffset < selection.focusOffset && textNode === selection.anchorNode && selection.anchorOffset === matchIndex - leadingSpaces.length || selection.anchorOffset > selection.focusOffset && textNode === selection.focusNode && selection.focusOffset === matchIndex - leadingSpaces.length;
-    //
-    //   if (!isUsersSelection) {
-    //     const trimmedTextNode = textNode.splitText(matchIndex);
-    //     const remainingTextNode = trimmedTextNode.splitText(selectionString.length);
-    //     const highlightedNode = highlightedMarkTemplate.cloneNode(true);
-    //     highlightedNode.appendChild(trimmedTextNode.cloneNode(true));
-    //     const parent = trimmedTextNode.parentNode;
-    //     if (parent) parent.replaceChild(highlightedNode, trimmedTextNode);
-    //     const otherHighlightedNodes = highlightOccurrences(remainingTextNode) || [];
-    //     return [highlightedNode].concat(otherHighlightedNodes);
-    //   } else {
-    //     const clonedNode = textNode.cloneNode();
-    //     const remainingClonedTextNode = clonedNode.splitText(matchIndex + selectionString.length);
-    //     if (occurrenceRegex.exec(remainingClonedTextNode.data)) return highlightOccurrences(textNode.splitText(matchIndex + selectionString.length));
-    //   }
-    // }
   }
 
   ;
